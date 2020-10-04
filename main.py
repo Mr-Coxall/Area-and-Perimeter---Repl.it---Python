0c39375a2bd250a2d05c156a7849b22afc32320a
Patrick Coxall <mr.coxall@mths.ca>
2020-10-04 20:30:19 +0000

    Fixed formatting

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ area = 0
 perimeter = 0
 
 # Input
-length = int(input("Enter the length of the rectangle: "))
-width = int(input("Enter the width of the rectangle: "))
+length = int(input("Enter the length of the rectangle (cm): "))
+width = int(input("Enter the width of the rectangle (cm): "))
 
 # Process
 perimeter = 2 * (length + width)
 area = length * width
 
 # output
-print("The area of your rectangle is: " + str(area))
-print("The perimeter of your rectangle is: " + str(perimeter))
+print("The area of your rectangle is: " + str(area) + "cm².")
+print("The perimeter of your rectangle is: " + str(perimeter) + "cm.")
 print("Done")
